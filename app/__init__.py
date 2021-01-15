e28e61f51c3781c929c0261f922867d0c6db5e11
Bryan Respass <bryres@bergen.org>
2021-01-15 11:06:25 -0500

    eb deployment

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,16 @@ from flask import Flask
 from flaskext.mysql import MySQL
 
 from config import Config
+import logging.handlers
+
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Handler 
+LOG_FILE = '/tmp/sample-app.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
